e1fa7fd11ed68d598cfe29a11a1754391921498d
bourbonut <ben.bourbon06@gmail.com>
2022-12-31 19:08:49 +0100

    Gear profile with interference (draft)

--- manim/involute.py
+++ manim/involute.py
@@ -40,11 +40,11 @@ def revolution(vgroup, angle, times):
 
 
 def rackprofile(m, alpha=radians(20), ka=1, kf=1.25):
-    ha = m * ka  # adendum height
-    hf = m * kf  # deddendum height
+    ha = m * ka  # addendum height
+    hf = m * kf  # dedendum height
     p = pi * m  # step
-    la = p * 0.5 - 2 * tan(alpha) * ha  # adendum length of tooth
-    lf = p * 0.5 - 2 * tan(alpha) * hf  # deddendum length of tooth
+    la = p * 0.5 - 2 * tan(alpha) * ha  # addendum length of tooth
+    lf = p * 0.5 - 2 * tan(alpha) * hf  # dedendum length of tooth
 
     #    B ____ C
     #     /    \
@@ -77,22 +77,28 @@ def v(t):
 def involute(t, r):
     return r * (u(t) - t * v(t))
 
+def involute2(t, r, t0):
+    return r * (u(t - t0) - t * v(t - t0))
 
-def inference_curve(t, r, x, y):
-    ut, vt = u(t), v(t)
-    return r * (ut - t * vt) + x * ut + y * vt
-
-
-def derived_involute(t, r):
-    return r * t * u(t)
+def interference_curve(t, r, x, t0):
+    return involute2(t, r, t0) - x * u(t)
 
 
-def derived_inference_curve(t, r, x, y):
-    ut, vt = u(t), v(t)
-    return r * t * u(t) + x * vt - y * ut
+def derived_involute(t, r, t0):
+    return r * t * u(t - t0)
 
 
-def gearprofile(m, z, alpha=radians(20), ka=1, kf=1.25, inference=False):
+def derived_interference_curve(t, r, x, t0):
+    return derived_involute(t, r, t0) - x * v(t)
+
+
+def jacobian_involute(rb, rp, x, t0):
+    return lambda t1, t2: mat3(
+        derived_involute(t1, rb, t0), -derived_interference_curve(t2, rp, x, t0), Z
+    )
+
+
+def gearprofile(m, z, alpha=radians(20), ka=1, kf=1.25, interference=False):
     ha = m * ka  # adendum height
     hf = m * kf  # deddendum height
     p = pi * m  # step
@@ -104,16 +110,51 @@ def gearprofile(m, z, alpha=radians(20), ka=1, kf=1.25, inference=False):
     ta = sqrt(ra * ra / (rb * rb) - 1)
     tp = sqrt(rp * rp / (rb * rb) - 1)
 
-    if inference:
-        pass
+    ha = ka * m
+    la = p * 0.5 - 2 * tan(alpha) * ha  # addendum length of tooth
+        
+    if interference:
+        ts = tp - atan2(tp, 1)
+        phase = PI / z + 2 * (tp - atan2(tp, 1))
+        phase_empty = 2 * PI / z - phase
+        sl = asin(la * 0.5 / rb) # rb
+        # sl = asin(la * 0.5 / (rp - ka * m)) # rb
+        f = lambda t1, t2: involute(t1, rb) - interference_curve(t2, rp, ka * m, phase_empty * 0.5 - sl)
+        J = jacobian_involute(rb, rp, ka * m, phase_empty * 0.5 - sl)
+        t1, t2, t3 = 0.5 * ta, -0.5 * ta, 0
+        for i in range(4):
+            t1, t2, t3 = vec3(t1, t2, t3) - inverse(J(t1, t2)) * f(t1, t2)
+
+        side1 = ParametricFunction(lambda x: involute(x, rb), t_range=[t1, ta])
+        interference1 = ParametricFunction(
+            lambda t: interference_curve(t, rp, ka * m, phase_empty * 0.5 - sl), t_range=[t2, 0]
+        )
+        side2 = side1.copy().apply_matrix(mat3(X, -Y, Z)).rotate_about_origin(phase)
+        interference2 = (
+            interference1.copy().apply_matrix(mat3(X, -Y, Z)).rotate_about_origin(phase)
+        )
+        angle_a = ta - atan2(ta, 1)
+        top = Arc(ra, angle_a, phase - 2 * angle_a)
+        M = interference_curve(0, rp, ka * m, phase_empty * 0.5 - sl)
+        angle_M = anglebt(M, u(0.5 * phase)) * 2
+        bottom = Line(M, mat3(u(-2 * PI / z + angle_M), v(-2 * PI / z + angle_M), Z) * M)
+
+        i = ParametricFunction(lambda t: interference_curve(t, rp, ka * m, phase_empty * 0.5 - sl), t_range=[-0.5 * ta, 0.5 * ta], color=RED)
+        h = ParametricFunction(lambda t: involute2(t, rp, 0.5 * phase_empty - sl), t_range=[-0.5 * ta, 0.5 * ta], color=YELLOW)
+        # return VGroup(
+        #     side1, interference1, side2, interference2, top, bottom, i, Line(O, 2 * ra * u(-0.5 * phase_empty + sl)), h, Line(O, rb * X)
+        # ).rotate_about_origin(-phase * 0.5)
+        return VGroup(
+            side1, interference1, side2, interference2, top, bottom,
+        ).rotate_about_origin(-phase * 0.5)
     else:
         tb = 0
         phase = PI / z + 2 * (tp - atan2(tp, 1))
         side1 = ParametricFunction(lambda x: involute(x, rb), t_range=[tb, ta])
         side2 = side1.copy().apply_matrix(mat3(X, -Y, Z)).rotate_about_origin(phase)
-        alpha = ta - atan2(ta, 1)
+        angle_a = ta - atan2(ta, 1)
         top = ParametricFunction(
-            lambda t: ra * u(t), t_range=[0.96 * alpha, 1.02 * (phase - alpha)]
+            lambda t: ra * u(t), t_range=[0.96 * angle_a, 1.02 * (phase - angle_a)]
         )
 
         small_radius = 0.5 * (rb - rf)
@@ -142,20 +183,22 @@ def gearprofile(m, z, alpha=radians(20), ka=1, kf=1.25, inference=False):
             arc_after,
             arc_before,
             bottom,
-        )
+        ).rotate_about_origin(-phase * 0.5)
 
 
 class Test(Scene):
     def construct(self):
-        # wire = rackprofile(2)
-        # self.add(repeat(wire, 3, 2))
-        # self.add(wire)
-        func = ParametricFunction(lambda x: involute(x, 1), t_range=[0, PI])
-        # func2 = ParametricFunction(lambda x: involute(x, 1), t_range=[0, PI])
-        # self.add(func.rotate_about_origin(PI / 2))
-        # self.add(func2.apply_matrix(mat3(X, -Y, Z)))
-        # self.add(func)
-        self.add(revolution(gearprofile(0.5, 12), 2 * PI / 12, 12))
+        m = 2
+        z = 24
+        rp = m * z * 0.5
+        l = -tan(radians(20)) * (1.25 * m)
+        la = pi * m * 0.5 - 2 * tan(radians(20)) * 1 * m  # addendum length of tooth
+        lf = pi * m * 0.5 - 2 * tan(radians(20)) * 1.25 * m  # dedendum length of tooth
+        wire = rackprofile(m)
+        g = VGroup(repeat(wire, 3, PI * m)) 
+        self.add(g.rotate_about_origin(PI / 2).shift(rp * X + (- PI * m - l + 0.5 * lf) * Y).shift(-rp * X)) # - (PI * m / 2 * Y)))
+        self.add(revolution(gearprofile(m, z, interference=True), 2 * PI / z, z).shift(-rp * X))# .rotate_about_origin(PI / z).shift(-rp * X))
+        # self.add(Line(-10 * X, 10 *X))
 
 
 class InvoluteFunction(Scene):
