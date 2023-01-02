a28483d2346accffa30c1282bd6aca4557c193ac
bourbonut <ben.bourbon06@gmail.com>
2023-01-02 18:50:47 +0100

    Better syntax for gear profile with interference

--- manim/utils/gear.py
+++ manim/utils/gear.py
@@ -1,8 +1,10 @@
 from manim import ParametricFunction, VGroup, Arc, Line, RED, YELLOW
 from glm import mat3, vec3, inverse
 from math import sqrt, radians, pi, cos, sin, tan, acos, asin, atan2
+from functools import partial
+from collections import namedtuple
 
-from .maths import u, v, O, X, Y, Z, anglebt
+from .maths import u, v, O, X, Y, Z, anglebt, rotation
 
 
 def involute(t, r, t0=0):
@@ -27,6 +29,18 @@ def jacobian_involute(rb, rp, x, y, t0):
     )
 
 
+def pitch_radius(m, z):
+    return m * z * 0.5
+
+
+def base_radius(m, z, alpha):
+    return pitch_radius(m, z) * cos(alpha)
+
+
+def angle_involute(r, rb):
+    return sqrt(r * r / (rb * rb) - 1)
+
+
 def profile(m, z, alpha=radians(20), ka=1, kf=1.25, interference=False):
     ha = m * ka  # adendum height
     hf = m * kf  # deddendum height
@@ -36,67 +50,52 @@ def profile(m, z, alpha=radians(20), ka=1, kf=1.25, interference=False):
     rf = rp - hf
     rb = rp * cos(alpha)
 
-    ta = sqrt(ra * ra / (rb * rb) - 1)
-    tp = sqrt(rp * rp / (rb * rb) - 1)
+    ta = angle_involute(ra, rb)
+    tp = angle_involute(rp, rb)
 
-    ha = ka * m
     la = p * 0.5 - 2 * tan(alpha) * ha  # addendum length of tooth
 
+    duplicate = (
+        lambda obj, angle: obj.copy()
+        .apply_matrix(mat3(X, -Y, Z))
+        .rotate_about_origin(angle)
+    )
+
     if interference:
+        Function = namedtuple("Function", ["involute", "interference"])
         ts = tp - atan2(tp, 1)
         phase = pi / z + 2 * (tp - atan2(tp, 1))
         phase_empty = 2 * pi / z - phase
-        sl = atan2(la * 0.5, (rp - ka * m))
-        f = lambda t1, t2: involute(t1, rb) - interference_curve(
-            t2, rp, ka * m, 0.5 * la, phase_empty * 0.5
+        angle_top = ta - atan2(ta, 1)
+        tmin = la * 0.5 / rp
+
+        functions = Function(
+            partial(involute, r=rb),
+            partial(interference_curve, r=rp, x=ha, y=0.5 * la, t0=phase_empty * 0.5),
         )
-        J = jacobian_involute(rb, rp, ka * m, 0.5 * la, phase_empty * 0.5)
+
+        # Newton method
+        f = lambda t1, t2: functions.involute(t1) - functions.interference(t2)
+        J = jacobian_involute(rb, rp, ha, 0.5 * la, phase_empty * 0.5)
         t1, t2, t3 = 0.5 * ta, -0.5 * ta, 0
         for i in range(8):
             t1, t2, t3 = vec3(t1, t2, t3) - inverse(J(t1, t2)) * f(t1, t2)
 
-        side1 = ParametricFunction(lambda x: involute(x, rb), t_range=[t1, ta])
-        tmin = la * 0.5 / rp
-        interference1 = ParametricFunction(
-            lambda t: interference_curve(t, rp, ka * m, 0.5 * la, phase_empty * 0.5),
-            t_range=[t2, tmin],
-        )
-        side2 = side1.copy().apply_matrix(mat3(X, -Y, Z)).rotate_about_origin(phase)
-        interference2 = (
-            interference1.copy().apply_matrix(mat3(X, -Y, Z)).rotate_about_origin(phase)
-        )
-        angle_a = ta - atan2(ta, 1)
-        top = Arc(ra, angle_a, phase - 2 * angle_a)
-        M = interference_curve(tmin, rp, ka * m, 0.5 * la, phase_empty * 0.5)
-        angle_M = anglebt(M, u(0.5 * phase)) * 2
-        bottom = Line(
-            M, mat3(u(-2 * pi / z + angle_M), v(-2 * pi / z + angle_M), Z) * M
-        )
+        # Involute and interference curve
+        side = ParametricFunction(functions.involute, t_range=[t1, ta])
+        interference = ParametricFunction(functions.interference, t_range=[t2, tmin])
+        duplicated_objs = map(partial(duplicate, angle=phase), (side, interference))
 
-        i = ParametricFunction(
-            lambda t: interference_curve(t, rp, ka * m, 0.5 * la, phase_empty * 0.5),
-            t_range=[-ta, ta],
-            color=RED,
-        )
-        h = ParametricFunction(
-            lambda t: involute(t, rp, 0.5 * phase_empty - sl),
-            t_range=[-0.5 * ta, 0.5 * ta],
-            color=YELLOW,
-        )
+        # Top and bottom
+        top = Arc(ra, angle_top, phase - 2 * angle_top)
+        M = functions.interference(tmin)
+        angle_bottom = anglebt(M, u(0.5 * phase)) * 2
+        bottom = Line(M, rotation(-2 * pi / z + angle_bottom) * M)
+
         return VGroup(
-            side1,
-            interference1,
-            side2,
-            interference2,
-            top,
-            bottom,
-            i,
-            # Line(O, (rp - ka * m) * u(-0.5 * phase_empty + sl)),
-            h,
+            side, interference, *duplicated_objs, top, bottom
         ).rotate_about_origin(-phase * 0.5)
-        # return VGroup(
-        #     side1, interference1, side2, interference2, top, bottom,
-        # ).rotate_about_origin(-phase * 0.5)
+
     else:
         tb = 0
         phase = pi / z + 2 * (tp - atan2(tp, 1))
