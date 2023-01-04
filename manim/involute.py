3f8d6920b555e59f3ecb02217189efbd51753d66
bourbonut <ben.bourbon06@gmail.com>
2023-01-04 18:51:47 +0100

    Updated involute drawing animation

--- manim/involute.py
+++ manim/involute.py
@@ -1,107 +1,78 @@
 from manim import *
 from utils import *
+from utils.maths import O, X, Y, Z
 from glm import *
 import numpy as np
 from math import radians, pi, cos, sin, tan, acos, asin, atan2
 from utils import rack, gear, maths, fast
 
-X = maths.X
-Y = maths.Y
 
 class InvoluteFunction(Scene):
-    def involute(self, t):
-        x = vec3(cos(t), sin(t), 0)
-        y = vec3(-x[1, x[0], 0])
-        return self.radius * (x + y * (self.t0 - t))
-
     def construct(self):
         # Parameters
-        self.t0 = 0
-        self.t_offset = 0
-        self.radius = 1
+        m, z, alpha = 1, 12, radians(20)
+        rb = gear.base_radius(m, z, alpha)
+        self.t = 0
         rate = 0.5
-        origin = O
+        offset = -rb * X - 0.55 * rb * Y
 
-        axes = Axes(axis_config={"include_ticks": False})
+        # Graph configuration
+        axes = Axes(x_length=14, y_length=14, axis_config={"include_ticks": False}).shift(offset)
         axes_labels = axes.get_axis_labels()
-        dotc = Dot(X * self.radius, color=RED)
-        doti = Dot(X * self.radius, color=BLUE)
-        circle = Circle(self.radius)
-        rot90 = rotation(pi * 0.5)
 
-        self.curve = VGroup()
-        self.curve.add(Line(doti.get_center(), doti.get_center()))
+        # Dots
+        on_circle = Dot(rb * maths.u(self.t), color=RED).shift(offset)
+        on_involute = Dot(gear.involute(self.t, rb), color=BLUE).shift(offset)
+
+        # Base circle
+        circle = Circle(rb).shift(offset)
+
+        # Involute curve
+        self.curve = VGroup(Line(on_involute.get_center(), on_involute.get_center()))
 
         def go_around_circle(mob, dt):
-            self.t_offset += dt * rate
-            mob.move_to(vec3(cos(self.t_offset), sin(self.t_offset), 0))
+            self.t += dt * rate
+            mob.move_to(rb * maths.u(self.t)).shift(offset)
 
         def go_line_circle():
-            return Line(origin, dotc.get_center())
+            return Line(O + offset, on_circle.get_center())
 
         def go_around_involute(mob, dt):
-            mob.move_to(self.involute(self.t_offset))
+            mob.move_to(gear.involute(self.t, rb)).shift(offset)
 
         def go_line_involute():
-            p1, p2 = dotc.get_center(), doti.get_center()
-            line = Line(p1, p2)
-            # if self.t_offset > pi * 0.25:
-            #     middle = (p1 + p2) * 0.5
-            #     v = p2 - p1
-            #     v = v / np.linalg.norm(v)
-            #     d = 0.4 * rot90 @ v
-            #     return VGroup(line, MathTex(r"t \times R", font_size=36).move_to(middle + d))
-            # else:
-            return line
+            p1, p2 = on_circle.get_center(), on_involute.get_center()
+            return Line(p1, p2)
 
         def draw_right_angle():
-            line1, line2 = Line(origin, dotc.get_center()), Line(
-                dotc.get_center(), doti.get_center()
+            line1, line2 = Line(O + offset, on_circle.get_center()), Line(
+                on_circle.get_center(), on_involute.get_center()
             )
-            inter = np.cross(
-                line1.get_end() - line1.get_start(), line2.get_end() - line2.get_start()
+            inter = cross(
+                line1.get_end() - line1.get_start(),
+                line2.get_end() - line2.get_start(),
             )
-            class_ = (
-                Line
-                if inter[2] == 0.0
-                else lambda l1, l2: RightAngle(
-                    l1, l2, length=min(0.2, 0.6 * self.t_offset), quadrant=(-1, 1)
-                )
+            rightangle_function = lambda l1, l2: RightAngle(
+                l1, l2, length=min(0.2, 0.6 * self.t), quadrant=(-1, 1)
             )
+            class_ = Line if inter[2] == 0.0 else rightangle_function
             return class_(line1, line2)
 
         def draw_involute():
             last_line = self.curve[-1]
-            new_line = Line(
-                last_line.get_end(), self.involute(self.t_offset), color=BLUE
-            )
+            new_line = Line(last_line.get_end(), gear.involute(self.t, rb) + offset, color=BLUE)
             self.curve.add(new_line)
             return self.curve
 
-        def draw_angle():
-            line1, line2 = Line(origin, origin + X), Line(origin, dotc.get_center())
-            inter = np.cross(
-                line1.get_end() - line1.get_start(), line2.get_end() - line2.get_start()
-            )
-            class_ = Line if inter[2] == 0.0 else Angle
-            # if self.t_offset > pi * 0.25:
-            #     angle = class_(line1, line2, radius = 0.5 + 3 * SMALL_BUFF, other_angle=False)
-            #     return VGroup(class_(line1, line2), MathTex(r"t", font_size=36).move_to(angle.point_from_proportion(0.5)))
-            # else:
-            return class_(line1, line2)
-
-        dotc.add_updater(go_around_circle)
-        doti.add_updater(go_around_involute)
+        on_circle.add_updater(go_around_circle)
+        on_involute.add_updater(go_around_involute)
 
         origin2circle = always_redraw(go_line_circle)
         circle2involute = always_redraw(go_line_involute)
         rightangle = always_redraw(draw_right_angle)
-        angle = always_redraw(draw_angle)
         involute = always_redraw(draw_involute)
 
         self.add(axes, axes_labels, circle)
-        self.add(dotc, doti)
-        self.add(origin2circle, circle2involute, angle, rightangle, involute)
-        self.wait(10)
-        # func = ParametricFunction(self.involute, t_range=vec3(0., 2.*pi), fill_opacity=0)
-        # self.add(func)
+        self.add(on_circle, on_involute)
+        self.add(origin2circle, circle2involute, rightangle, involute)
+        self.wait(3.5)
