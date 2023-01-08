772745429172cff5a1c796d15a023d2f784782df
bourbonut <ben.bourbon06@gmail.com>
2023-01-08 17:49:25 +0100

    Added script to copy quickly files

---
+++ manim/quick-copy.sh
@@ -0,0 +1,11 @@
+PRJROOT=".."
+MEDIA=$PRJROOT/manim/media
+POST=$PRJROOT/posts/welcome
+
+force=$MEDIA/images/force/Gear2Gear_ManimCE_v0.17.2.png
+static_involute=$MEDIA/images/involute/Static_ManimCE_v0.17.2.png
+video_involute=$MEDIA/videos/involute/720p30/InvoluteFunction.mp4
+
+cp $force $POST/force.png
+cp $static_involute $POST/static_involute.png
+cp $video_involute $POST
