e840e5152180e159b1eb443332cb2bb7b2ab2d49
bourbonut <ben.bourbon06@gmail.com>
2023-01-14 14:55:21 +0100

    Updated colors and rack dimensions

--- manim/rack_gear.py
+++ manim/rack_gear.py
@@ -72,8 +72,8 @@ def gear_information(interference=False):
     rf = gear.dedendum_radius(m, z, kf)
 
     # Circles
-    circle_pitch = Circle(rp, color=RED, stroke_width=2)
-    circle_base = Circle(rb, color=YELLOW, stroke_width=2)
+    circle_pitch = Circle(rp, color=YELLOW, stroke_width=2)
+    circle_base = Circle(rb, color=RED, stroke_width=2)
     circle_addendum = DashedVMobject(Circle(ra, color=BLUE, stroke_width=2))
     circle_dedendum = DashedVMobject(Circle(rf, color=BLUE, stroke_width=2))
 
@@ -81,8 +81,8 @@ def gear_information(interference=False):
     center = Dot(O)
 
     # Arrows
-    pitch_arrow = Line(O, rp * maths.u(PI / 3), color=RED)
-    base_arrow = Line(O, rb * maths.u(2 * PI / 3), color=YELLOW)
+    pitch_arrow = Line(O, rp * maths.u(PI / 3), color=YELLOW)
+    base_arrow = Line(O, rb * maths.u(2 * PI / 3), color=RED)
     addendum_arrow = Line(O, ra * maths.u(PI / 6), color=BLUE)
     dedendum_arrow = Line(O, rf * maths.u(PI - PI / 6), color=BLUE)
 
@@ -92,10 +92,10 @@ def gear_information(interference=False):
     dedendum_arrow.add_tip(tip_shape=ArrowTriangleFilledTip)
 
     # Text
-    rp_text = MathTex("r_p", color=RED).move_to(
+    rp_text = MathTex("r_p", color=YELLOW).move_to(
         pitch_arrow.get_center() + 0.1 * ra * maths.v(PI / 3)
     )
-    rb_text = MathTex("r_b", color=YELLOW).move_to(
+    rb_text = MathTex("r_b", color=RED).move_to(
         base_arrow.get_center() + 0.1 * ra * maths.v(2 * PI / 3)
     )
     ra_text = MathTex("r_a", color=BLUE).move_to(
@@ -142,43 +142,85 @@ class Rack(Scene):
         ka, kf = 1, 1.25
         ha = rack.addendum_height(m, ka)
         hf = rack.addendum_height(m, kf)
+        la = rack.addendum_length(m, alpha, ka)
         line = lambda y, color: Line(
             y * Y, 3 * step * X + y * Y, color=color, stroke_width=2
         )
         A = step * X - hf * Y
+        C = (step + (ha + hf) * tan(alpha) + 0.5 * la) * X
+        M = C + ha * Y
+        P = M + 0.5 * la * X
+        extra = -0.5 * step * X
 
-        # Double arrows because Manim has tips bugged
+        # Rack
+        rackprofile = fast.repeat(rack.profile(m), 3, step)
+
+        # Lines
+        pitch_line = line(0, YELLOW)
+        addedumn_line = DashedVMobject(line(ha, BLUE))
+        dedendum_line = DashedVMobject(line(-hf, BLUE))
+        extra_lines = [
+            DashedVMobject(Line(extra, O)),
+            DashedVMobject(Line(extra + ha * Y, ha * Y)),
+            DashedVMobject(Line(extra - hf * Y, -hf * Y)),
+        ]
+        alpha_line1 = DashedVMobject(
+            Line(
+                A + (ha + hf) * maths.u(PI / 2 - alpha),
+                A + (3 * ha + hf) * maths.u(PI / 2 - alpha),
+            )
+        )
+        alpha_line2 = DashedVMobject(
+            Line(A, A + (3 * ha + hf) * sin(PI / 2 - alpha) * Y)
+        )
+
+        # Dots
+        center = Dot(C)
+        middle = Dot(M)
+        corner = Dot(P)
+
+        # Double arrows
+        # Custom class because Manim has tips bugged
+        ha_arrow = CustomDoubleArrow(0.5 * extra, 0.5 * extra + ha * Y, tip_length=0.2, tip_width=0.2)
+        hf_arrow = CustomDoubleArrow(0.5 * extra, 0.5 * extra - hf * Y, tip_length=0.2, tip_width=0.2)
+        alpha_arrow = CurvedDoubleArrow(
+            A + (2 * ha + hf) * maths.u(PI / 2 - alpha),
+            A + (2 * ha + hf) * Y,
+            tip_length=0.2,
+            radius=2 * ha + hf,
+        )
+        X_arrow = CustomDoubleArrow(C, M, tip_width=0.15, tip_length=0.15)
+        Y_arrow = CustomDoubleArrow(M, P, tip_width=0.15, tip_length=0.15)
+
+        # Text
+        ha_text = MathTex("h_a").move_to(-0.4 * step * X + 0.5 * ha * Y)
+        hf_text = MathTex("h_f").move_to(-0.4 * step * X - 0.5 * hf * Y)
+        alpha_text = MathTex("\\alpha").move_to(
+            A + (2.5 * ha + hf) * maths.u(PI / 2 - 0.5 * alpha)
+        )
+        X_text = MathTex("X", font_size=28).move_to((C + P) * 0.5)
+        Y_text = MathTex("Y", font_size=28).move_to((C + P) * 0.5 * (X + 2.5 * Y))
+
         objs = VGroup(
-            fast.repeat(rack.profile(m), 3, step),
-            line(0, YELLOW),
-            DashedVMobject(line(ha, BLUE)),
-            DashedVMobject(line(-hf, BLUE)),
-            DashedVMobject(Line(-0.5 * step * X, O)),
-            DashedVMobject(Line(-0.5 * step * X + ha * Y, ha * Y)),
-            DashedVMobject(Line(-0.5 * step * X - hf * Y, -hf * Y)),
-            CustomDoubleArrow(
-                -0.25 * step * X, -0.25 * step * X + ha * Y, tip_length=0.2
-            ),
-            CustomDoubleArrow(
-                -0.25 * step * X, -0.25 * step * X - hf * Y, tip_length=0.2
-            ),
-            MathTex("h_a").move_to(-0.4 * step * X + 0.5 * ha * Y),
-            MathTex("h_f").move_to(-0.4 * step * X - 0.5 * hf * Y),
-            DashedVMobject(
-                Line(
-                    A + (ha + hf) * maths.u(PI / 2 - alpha),
-                    A + (3 * ha + hf) * maths.u(PI / 2 - alpha),
-                )
-            ),
-            DashedVMobject(Line(A, A + (3 * ha + hf) * sin(PI / 2 - alpha) * Y)),
-            CurvedDoubleArrow(
-                A + (2 * ha + hf) * maths.u(PI / 2 - alpha),
-                A + (2 * ha + hf) * Y,
-                tip_length=0.2,
-                radius=2 * ha + hf,
-            ),
-            MathTex("\\alpha").move_to(
-                A + (2.5 * ha + hf) * maths.u(PI / 2 - 0.5 * alpha)
-            ),
+            rackprofile,
+            pitch_line,
+            addedumn_line,
+            dedendum_line,
+            alpha_line1,
+            alpha_line2,
+            *extra_lines,
+            center,
+            middle,
+            corner,
+            ha_arrow,
+            hf_arrow,
+            alpha_arrow,
+            X_arrow,
+            Y_arrow,
+            ha_text,
+            hf_text,
+            alpha_text,
+            X_text,
+            Y_text,
         )
         self.add(objs.shift(-1.25 * step * X))
