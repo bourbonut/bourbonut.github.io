2399f8a7de086b35062e0dce7f1710c2f859d40f
bourbonut <ben.bourbon06@gmail.com>
2023-02-20 18:50:41 +0100

    Added primes sum project

---
+++ projects/performance/grab_code.py
@@ -0,0 +1,56 @@
+import requests
+from operator import itemgetter
+from itertools import starmap
+
+html = """---
+title: "Computation of the sum of prime numbers in different languages"
+author: "Benjamin Bourbon"
+date: "2023-01-20"
+categories: [computing]
+---
+
+<script src="https://d3js.org/d3.v4.js"></script>
+
+{}
+
+{}
+
+<div id="standard"></div>
+<div id="logarithmic"></div>
+
+<script src="https://d3js.org/d3-scale-chromatic.v1.min.js"></script>
+<script src="plot.js"></script>
+<script src="selection.js"></script>
+"""
+
+raw_url = "https://raw.githubusercontent.com/bourbonut/prime_numbers_sum/main/programs/{}/primes.{}"
+
+
+def grab_code(language, suffix):
+    response = requests.get(raw_url.format(language, suffix))
+    content = response.content.decode("utf-8")
+    return f"```{language}\n{content}```"
+
+def button(language):
+    title = language.title() if language != "cpp" else "C++"
+    return f"""<input id="{language}-button" type="button" value="{title}" onclick="select_language('{language}');"></input>"""
+
+languages = [
+    ("python", "py"),
+    ("rust", "rs"),
+    ("cpp", "cpp"),
+    ("elixir", "ex"),
+    ("java", "java"),
+    ("lua", "lua"),
+    ("javascript", "js"),
+    ("scala", "scala"),
+]
+
+# print(grab_code("python", "py"))
+code = "\n\n".join(starmap(grab_code, languages))
+buttons = "\n".join(map(button, map(itemgetter(0), languages)))
+
+content = html.format(buttons, code)
+with open("index.qmd", "w") as file:
+    file.write(content)
+
