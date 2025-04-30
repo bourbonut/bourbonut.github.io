803f24aabaf7406caa139f3bdf4291cb38741226
bourbonut <ben.bourbon06@gmail.com>
2025-04-30 17:39:55 +0200

    Add `BevelTransmission` and functions to generate a complete spherical gear profile

--- manim/bevel_gear.py
+++ manim/bevel_gear.py
@@ -31,10 +31,10 @@ def get_pitch_cone_angle(
 
 
 def spherical_involute(cone_angle:float, t0:float, t:float) -> vec3:
-	"""
-	Return spherical involute function
+    """
+    Return spherical involute function
 
-	Parameters
+    Parameters
     ----------
     t : float
         The angular position
@@ -43,17 +43,17 @@ def spherical_involute(cone_angle:float, t0:float, t:float) -> vec3:
     cone_angle : float
         The cone angle
 
-	Returns
+    Returns
     -------
     vec3
-		A point on the spherical involute
-	"""
-	cos_g, sin_g = cos(cone_angle), sin(cone_angle)
-	return vec3(
-		sin_g * cos(t * sin_g) * cos(t + t0) + sin(t * sin_g) * sin(t + t0),
-		sin_g * cos(t * sin_g) * sin(t + t0) - sin(t * sin_g) * cos(t + t0),
-		cos_g * cos(t * sin_g),
-	)
+        A point on the spherical involute
+    """
+    cos_g, sin_g = cos(cone_angle), sin(cone_angle)
+    return vec3(
+        sin_g * cos(t * sin_g) * cos(t + t0) + sin(t * sin_g) * sin(t + t0),
+        sin_g * cos(t * sin_g) * sin(t + t0) - sin(t * sin_g) * cos(t + t0),
+        cos_g * cos(t * sin_g),
+    )
 
 class BevelGearSection(Scene):
     def construct(self):
@@ -1322,4 +1322,362 @@ class BevelStaticInvolute(ThreeDScene):
             rb_group,
             theta_tex,
             system,
+        )
+
+def spherical_involuteof(pitch_cone_angle:float, t0:float, alpha:float, t:float) -> vec3:
+    """
+    Return the spherical interference function
+
+    Parameters
+    ----------
+    t : float
+        The angular position
+    t0 : float
+        The difference phase
+    pitch_cone_angle : float
+        The pitch cone angle
+    alpha : float
+        The height angle offset of the rack
+
+    Returns
+    -------
+        a normalized `vec3`
+    """
+    cos_p, sin_p = cos(pitch_cone_angle), sin(pitch_cone_angle)
+    return (
+        cos(alpha) * spherical_involute(pitch_cone_angle, t0, t) 
+        + sin(alpha) * vec3(-cos_p * cos(t + t0), -cos_p * sin(t + t0), sin_p)
+    )
+
+
+def derived_spherical_involute(cone_angle:float, t0:float):
+    """
+    Return the function of the derived spherical involute function.
+
+    Parameters
+    ----------
+    cone_angle : float
+        The cone angle
+    t0 : float
+        The phase difference
+    """
+    cos_g, sin_g = cos(cone_angle), sin(cone_angle)
+    return lambda t: vec3(
+        cos_g ** 2 * sin(t * sin_g) * cos(t + t0),
+        cos_g ** 2 * sin(t * sin_g) * sin(t + t0),
+        -cos_g * sin_g * sin(t * sin_g),
+    )
+
+
+def jacobian_spherical_involute(base_cona_angle:float, pitch_cone_angle:float, t01:float, t02:float, alpha:float) -> callable:
+    """
+    Return the function of the jacobian used for the newton method in `spherical_gearprofile`
+
+    Parameters
+    ----------
+    base_cona_angle : float
+        The base cone angle
+    pitch_cone_angle : float
+        The pitch cone angle
+    t01 : float
+        The phase of the spherical involute function
+    t02 : float
+        The phase of the spherical interference function
+    alpha : float
+        The height angle offset of the rack
+    """
+    dsi = derived_spherical_involute # for convenience
+    derived_involute = dsi(base_cona_angle, t01)
+    cos_p = cos(pitch_cone_angle)
+    vec = lambda t: vec3(cos_p * sin(t), -cos_p * cos(t), 0)
+    derived_interference = lambda t: dsi(pitch_cone_angle, t02)(t) * cos(alpha) + sin(alpha) * vec(t + t02)
+    return lambda t1, t2: mat3(derived_involute(t1), -derived_interference(t2), vec3(0, 0, 1))
+
+
+def spherical_rack_tools(z:float, pressure_angle:float=pi / 9, ka:float=1, kd:float=1.25):
+    """
+    Return a list of all information useful to generate a spherical rack.
+
+    Parameters
+    ----------
+    z : float
+        Number of tooth of the rack equal to `z_pinion / sin(pitch_cone_angle)`
+        or `z_wheel / sin(shaft_angle - pitch_cone_angle)`
+    pressure_angle : float
+        The pressure angle of the gear
+    ka : float
+        The addendum coefficient
+    kd : float
+        The dedendum coefficient
+
+    Returns
+    -------
+    tuple
+        * the minimum abscissa for the function (fifth element)
+        * the maximum abscissa for the function (fifth element)
+        * the phase of a tooth
+        * the phase of space
+        * the function to generate a tooth
+    """
+    k = 1 / z
+    gamma_p = 0.5 * pi
+    gamma_b = asin(cos(pressure_angle))
+    cos_b, sin_b = cos(gamma_b), sin(gamma_b)
+    gamma_f = gamma_p + atan2(2 * ka * k, 1)
+    gamma_r = gamma_p - atan2(2 * kd * k, 1)
+
+    phi_p = acos(tan(gamma_b) / tan(gamma_p))
+    theta_p = atan2(sin_b * tan(phi_p), 1) / sin_b - phi_p
+    phase_diff = k * pi + 2 * theta_p
+
+    t_min = acos(cos(gamma_r) / cos_b) / sin_b
+    t_max = acos(cos(gamma_f) / cos_b) / sin_b
+
+    involute = lambda t, t0: spherical_involute(gamma_b, t0, t)
+    v = vec3(1, 1, 0)
+    phase_empty = 2 * pi * k - anglebt(involute(t_min, 0) * v, involute(-t_min, phase_diff) * v)
+
+    return [t_min, t_max, phase_diff, phase_empty, involute]
+
+def spherical_gearprofile(
+    z:int, 
+    step: float,
+    pitch_cone_angle:float, 
+    pressure_angle:float = pi / 9, 
+    ka:float = 1, 
+    kd:float = 1.25, 
+):
+    """
+    Generate 1-period tooth spherical profile for a bevel gear
+
+    Parameters
+    ----------
+    z : int
+        Number of tooth on the gear this profile is meant for
+    step : float
+        Step of a tooth
+    pitch_cone_angle : float
+        The pitch cone angle
+    pressure_angle : float
+        Pressure angle of the tooth
+    ka : float
+        Addendum coefficient
+    kd : float
+        Dedendum coefficient
+    """
+    # Initialization of parameters
+    gamma_p = pitch_cone_angle # for convenience
+    rp = z * step / (2 * pi)
+    rho1 = rp / sin(gamma_p)
+    gamma_b = asin(cos(pressure_angle) * sin(gamma_p))
+    cos_b, sin_b = cos(gamma_b), sin(gamma_b)
+    tooth_size = pi / z
+    involute = lambda t, t0 : spherical_involute(gamma_b, t0, t)
+    epsilon_p = acos(cos(gamma_p) / cos_b) / sin_b
+    theta_p = anglebt(involute(0, 0) * vec3(1, 1, 0), involute(epsilon_p, 0) * vec3(1, 1, 0))
+    phase_diff = tooth_size + 2 * theta_p
+    phase_empty = phase_interference = 2 * pi / z - phase_diff
+    # The following number `k` is useful to simplify some calculations
+    # It's broadly speaking `1/z_rack` and `z_rack` is not an integer !
+    k = sin(gamma_p) / z
+
+    # Spherical involute part
+    gamma_f = gamma_p + atan2(2 * ka * k, 1) # addendum cone angle
+    gamma_r = gamma_p - atan2(2 * kd * k, 1) # dedendum cone angle
+    t_min = 0
+    t_max = acos(cos(gamma_f) / cos_b) / sin_b
+    if gamma_r > gamma_b:
+        v = vec3(1, 1, 0)
+        t_min = acos(cos(gamma_r) / cos_b) / sin_b
+        phase_empty = 2 * pi / z - anglebt(
+            involute(t_min, 0) * v, involute(-t_min, phase_diff) * v
+        )
+
+    # Calculation of offsets due to geometry of spherical rack
+    _, t_rack_max, phase1, _, rinvolute = spherical_rack_tools(1 / k, pressure_angle, ka, kd)
+    interference = lambda t, t0 : spherical_involuteof(gamma_p, t0, alpha, t)
+    alpha = 2 * ka * k
+    n1, n2 = rinvolute(t_rack_max, 0) * vec3(1, 1, 0), rinvolute(-t_rack_max, phase1) * vec3(1, 1, 0)
+    beta = 0.5 * anglebt(n1, n2) * length(n1) / sin_b
+
+    # Newton method to calculate the intersection between
+    # the spherical involute and the spherical interference.
+    # Objective function
+    involuteat = lambda t2, t0 : spherical_involuteof(gamma_p, t0, alpha, t2)
+    f = lambda t1, t2: involute(t1, 0) - involuteat(t2, -0.5 * phase_interference + beta)
+    # Jacobian matrix
+    J = jacobian_spherical_involute(gamma_b, gamma_p, 0, -0.5 * phase_interference + beta, alpha)
+
+    # Compute the intersection values
+    t1, t2, t3 = 0.5 * t_max, -0.5 * t_max, 0
+    for i in range(8):
+        t1, t2, t3 = vec3(t1, t2, t3) - inverse(J(t1, t2)) * f(t1, t2)
+
+    # Build sides of a tooth
+    interference1 = ParametricFunction(
+        lambda t: rho1 * interference(t, -0.5 * phase_interference + beta),
+        t_range=[t2, 0],
+    )
+    interference2 = ParametricFunction(
+        lambda t: rho1 * interference(-t, phase_diff + 0.5 * phase_interference - beta),
+        t_range=[t2, 0],
+    )
+    side1 = ParametricFunction(lambda t: rho1 * involute(t, 0), t_range=[t1, t_max])
+    side2 = ParametricFunction(lambda t: rho1 * involute(-t, phase_diff), t_range=[t1, t_max])
+
+    # Extreme points of sides to compute angle between them
+    a = rho1 * interference(0, -0.5 * phase_interference + beta)
+    b = rho1 * interference(0, phase_diff + 0.5 * phase_interference - beta)
+    final_phase_empty = 2 * pi / z - anglebt(a * vec3(1, 1, 0), b * vec3(1, 1, 0))
+    top = Line(rho1 * involute(t_max, 0), rho1 * involute(-t_max, phase_diff))
+    bottom = Line(rotate(-final_phase_empty, vec3(0, 0, 1)) * a, a)
+
+    aligned_phase = anglebt(
+        (rho1 * involute(t_max, 0) + rho1 * involute(-t_max, phase_diff)) * 0.5 * vec3(1, 1, 0),
+        X,
+    )
+
+    return VGroup(
+        interference1,
+        interference2,
+        side1,
+        side2,
+        top,
+        bottom,
+        Dot(a, radius=0.03),
+        Dot(b, radius=0.03),
+        Dot(rho1 * involute(t1, 0), radius=0.03),
+        Dot(rho1 * involute(t_max, 0), radius=0.03),
+        Dot(rho1 * involute(-t1, phase_diff), radius=0.03),
+        Dot(rho1 * involute(-t_max, phase_diff), radius=0.03),
+        # Line(rotate(-final_phase_empty, vec3(0, 0, 1)) * a, rho1 * Z),
+        # Line(b, rho1 * Z),
+    ).rotate_about_origin(-aligned_phase, Z)
+
+class BevelTransmission(ThreeDScene):
+
+    def construct(self):
+        z_pinion = 10
+        z_wheel = 20
+        m = 0.5
+        step = pi * m
+        pressure_angle = pi / 9
+        pitch_cone_angle = get_pitch_cone_angle(z_pinion, z_wheel)
+
+        gamma_p = pitch_cone_angle # for convenience
+        rp = z_pinion * step / (2 * pi)
+        rho1 = rp / sin(gamma_p)
+
+        angle1tooth_pinion = 2 * pi / z_pinion
+        # pinion_profile = spherical_gearprofile(z_pinion, step, pitch_cone_angle)
+        pinion_profile = fast.revolution(spherical_gearprofile(z_pinion, step, pitch_cone_angle), angle1tooth_pinion, z_pinion)
+
+        angle1tooth_wheel = 2 * pi / z_wheel
+        # wheel_profile = spherical_gearprofile(z_wheel, step, pi / 2 - pitch_cone_angle)
+        wheel_profile = fast.revolution(spherical_gearprofile(z_wheel, step, pi / 2 - pitch_cone_angle), angle1tooth_wheel, z_wheel)
+
+        sphere = Sphere(radius=rho1, fill_color=DARK_GRAY).set_opacity(0.01)
+
+        # Pinion
+        gamma_pp = pitch_cone_angle
+        gamma_bp = asin(cos(pressure_angle) * sin(gamma_pp))
+        rpp = z_pinion * step / (2 * pi)
+        rho1 = rho1p = rpp / sin(gamma_pp)
+        rbp = rho1p * sin(gamma_bp)
+        phip = acos(tan(gamma_bp) / tan(gamma_pp))
+
+        # Wheel
+        gamma_pw = pi * 0.5 - pitch_cone_angle
+        gamma_bw = asin(cos(pressure_angle) * sin(gamma_pw))
+        rpw = z_wheel * step / (2 * pi)
+        rho1w = rpw / sin(gamma_pw)
+        rbw = rho1w * sin(gamma_bw)
+        phiw = acos(tan(gamma_bw) / tan(gamma_pw))
+
+        circles = VGroup(
+            # Circle(rho1, color=WHITE),
+            # Circle(rho1, color=WHITE).rotate(pi / 2, Y, about_point=O),
+            # Circle(rho1, color=WHITE).rotate(-pressure_angle, X, about_point=O),
+            Circle(rbw, color=RED)
+            .rotate_about_origin(pi / 2 - gamma_pw, Y)
+            .move_to(rho1 * cos(gamma_bw) * rotate(-gamma_pw, Y) * X),
+            DashedVMobject(Circle(rpw, color=RED))
+            .rotate_about_origin(pi / 2 - gamma_pw, Y)
+            .move_to(rho1 * cos(gamma_pw) * rotate(-gamma_pw, Y) * X),
+            Circle(rbp, color=BLUE)
+            .rotate_about_origin(pi / 2 + gamma_pp, Y)
+            .move_to(rho1 * cos(gamma_bp) * rotate(gamma_pp, Y) * X),
+            DashedVMobject(Circle(rpp, color=BLUE))
+            .rotate_about_origin(pi / 2 + gamma_pp, Y)
+            .move_to(rho1 * cos(gamma_pp) * rotate(gamma_pp, Y) * X),
+        )
+
+        lines = VGroup(
+            DashedLine(rho1 * X, -0.5 * rho1 * Y + rho1 * X),
+            Arrow3D(start=rho1 * X, end=-0.5 * rho1 * Y + rho1 * X, stroke_width=6)
+            .rotate(-pressure_angle, X, about_point=rho1 * X),
+        )
+
+        O1 = rho1 * rotate(-gamma_pw, Y) * X
+        O2 = rho1 * rotate(gamma_pp, Y) * X
+        P = rho1 * X
+        T1 = rotate(-phiw, O1) * rotate(-gamma_pw, Y) * (rho1 * cos(gamma_bw) * X - rbw * Z)
+        T2 = rotate(-phip, O2) * rotate(gamma_pp, Y) * (rho1 * cos(gamma_bp) * X + rbp * Z)
+
+        dots = VGroup(
+            Dot3D(P, color=WHITE),
+            Dot3D(O1, color=RED),
+            Dot3D(T1, color=RED),
+            Dot3D(O2, color=BLUE),
+            Dot3D(T2, color=BLUE),
+        )
+
+        tex = VGroup(
+            MathTex("O_1", color=RED)
+            .rotate(pi / 2, X, about_point=O)
+            .rotate(pi / 2, Z, about_point=O)
+            .next_to(O1, direction=-Z),
+            MathTex("O_2", color=BLUE)
+            .rotate(pi / 2, X, about_point=O)
+            .rotate(pi / 2, Z, about_point=O)
+            .next_to(O2, direction=Z),
+            MathTex("T_1", color=RED)
+            .rotate(pi / 2, X, about_point=O)
+            .rotate(pi / 2, Z, about_point=O)
+            .next_to(T1, direction=Z),
+            MathTex("T_2", color=BLUE)
+            .rotate(pi / 2, X, about_point=O)
+            .rotate(pi / 2, Z, about_point=O)
+            .next_to(T2, direction=-Z),
+            MathTex("P", color=WHITE)
+            .rotate(pi / 2, X, about_point=O)
+            .rotate(pi / 2, Z, about_point=O)
+            .next_to(P, direction=Z),
+            MathTex("\\overrightarrow{F_{2 / 1}}")
+            .rotate(pi / 2, X, about_point=O)
+            .rotate(pi / 2, Z, about_point=O)
+            .next_to(-rotate(-pressure_angle, X) * 0.5 * rho1 * Y, direction=normalize(Z + Y)),
+        )
+
+
+        # Arc for alpha
+        arcs = arc_with_arrows(
+            func=lambda t: -rotate(-pressure_angle * t, X) * 0.37 * rho1 * Y,
+            tex_content="\\alpha",
+            direction=-0.5 * Y,
+            theta_rot=pi / 2,
+            phi_rot=pi / 2,
+        )
+
+        self.set_camera_orientation(phi=90 * DEGREES, theta=00 * DEGREES, zoom=0.8, focal_distance=1000)
+        # self.add(sphere, pinion_profile, wheel_profile)
+        self.add(
+            sphere,
+            circles,
+            lines,
+            dots,
+            tex,
+            *arcs,
+            pinion_profile.rotate_about_origin(pi + pi / (2 * z_pinion), Z).rotate_about_origin(pi / 2 + gamma_p, Y),
+            wheel_profile.rotate_about_origin(pi / (2 * z_wheel), Z).rotate_about_origin(gamma_p, Y),
         )
